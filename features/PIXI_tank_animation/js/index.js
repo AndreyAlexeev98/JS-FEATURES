e87e268b40e9fe1b34aacc69bf8605472dc4c033
AndreyAlexeev98 <ipalexeev98@mail.ru>
2025-01-29 07:56:02 +0300

    feat: add handler on all stage

--- features/PIXI_tank_animation/js/index.js
+++ features/PIXI_tank_animation/js/index.js
@@ -1,4 +1,4 @@
-import { Application, Graphics } from "./pixi.mjs";
+import { Application, Graphics, Rectangle } from "./pixi.mjs";
 import { assetsMap } from "./assetsMap.js";
 import { Tank } from "./Tank.js";
 
@@ -22,7 +22,7 @@ const runGame = () => {
   //   tank._tracksLeft.play();
   //   tank._tracksRight.play();
 
-  app.stage.addChild(marker); // Добавили нарисованный на canvas элемент в контейнер
+  //   app.stage.addChild(marker); // Добавили нарисованный на canvas элемент в контейнер
   app.stage.position.set(800 / 2, 800 / 2); // изменили позиционирование добавляемых элементов относительноцентра контейнера
 
   //   tank.rotateTowerBy(50);
@@ -40,11 +40,22 @@ const runGame = () => {
   //     }, 2000);
   //   })();
 
-  //   const onPointerDown = (event) => {
-  //     console.log(event);
-  //   };
+  const onPointerDown = ({ data }) => {
+    console.log(data);
 
-  //   app.stage.on("pointerdown", onPointerDown, undefined);
+    const positions = data.getLocalPosition(app.stage);
+    app.stage.addChild(
+      new Graphics()
+        .beginFill(0xff0000, 1)
+        .drawCircle(positions.x, positions.y, 5)
+        .endFill()
+    );
+  };
+
+  app.stage.on("pointerdown", onPointerDown, undefined);
+  app.stage.interactive = true;
+  app.stage.interactiveChildren = false;
+  app.stage.hitArea = new Rectangle(-400, -400, 800, 800); // для обработки события по всему stage
 };
 
 assetsMap.sprites.forEach((value) => app.loader.add(value));
